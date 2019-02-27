5be977fa956d3435b7993aec8f5c12c5010413b4
Victor <44604893+victor110298@users.noreply.github.com>
2019-02-28 00:21:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,30 @@
-import java.util.Scanner;
+import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 
 public class Main {
+    private static int[] arr;
+    private static long start;
 
-    //Task 1.1
-
-    public static int matchScore(int realScore1, int realScore2, int score1, int score2) {
-        return (realScore1 == score1) && (realScore2 == score2) ? 2
-                : ((realScore1 > realScore2) && (score1 > score2))
-                || ((realScore1 < realScore2) && (score1 < score2)) ? 1 : 0;
+    public Main() {
     }
 
-
-    //Task 1.2
-
+    public static int matchScore(int realScore1, int realScore2, int score1, int score2) {
+        return realScore1 == score1 && realScore2 == score2 ? 2 : ((realScore1 <= realScore2 || score1 <= score2) && (realScore1 >= realScore2 || score1 >= score2) ? 0 : 1);
+    }
 
     public static void drawDiamond(int num) {
-        int prob;
         int z = 1;
 
-        for (int i = 0; i <= num / 2 + 1; ++i) {
+        int prob;
+        int i;
+        for(i = 0; i <= num / 2 + 1; ++i) {
             prob = (num - z) / 2;
 
-            for (i = 0; i <= prob; ++i) {
+            for(i = 0; i <= prob; ++i) {
                 System.out.print(" ");
             }
 
-            for (i = 0; i < z; ++i) {
+            for(i = 0; i < z; ++i) {
                 System.out.print("*");
             }
 
@@ -36,14 +34,14 @@ public class Main {
 
         z = num - 1;
 
-        for (int i = num / 2; i >= 0; --i) {
+        for(i = num / 2; i >= 0; --i) {
             prob = (num - z) / 2;
 
-            for (i = 0; i <= prob; ++i) {
+            for(i = 0; i <= prob; ++i) {
                 System.out.print(" ");
             }
 
-            for (i = 0; i <= z; ++i) {
+            for(i = 0; i <= z; ++i) {
                 System.out.print("*");
             }
 
@@ -53,79 +51,97 @@ public class Main {
 
     }
 
-
-//Task 1.3
-
-
     public static int powerNumber(int number, int power) {
         int result = number;
-        for (int i = 0; i < power - 1; i++) {
+
+        for(int i = 0; i < power - 1; ++i) {
             result *= number;
         }
+
         return result;
     }
 
-// Task 1.4
-
     public static int factorial(int n) {
-        if (n==1)
-            return 1;
-        else
-            return n*factorial(n-1);
+        return n == 1 ? 1 : n * factorial(n - 1);
     }
 
-
-    public static int GetFibo(int n) {
-        if (n > 1)
-            return GetFibo(n - 2) + GetFibo(n - 1);
-        else if (n == 1)
-            return 1;
-        else
-            return 0;
+    public static int getFibo(int n) {
+        if (n > 1) {
+            return getFibo(n - 2) + getFibo(n - 1);
+        } else {
+            return n == 1 ? 1 : 0;
+        }
     }
-
-
 
     public static void main(String[] args) {
-        System.out.println("Task 1.1");
-        matchScore(1, 0, 2, 1);
-
-        System.out.println("Task 1.2");
-        drawDiamond(7);
-
-        System.out.println("Task 1.3");
-        powerNumber(3, 5);
-
-        System.out.println("Task 1.4");
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" Введите  число для вычисления Факториал(число): ");
-        int n1 = sc.nextInt();
-        System.out.println("Спасибо! Теперь введите число(номер елементя ряда Фибоначчи : ");
-        int n2 = sc.nextInt();
-        System.out.println( " Результат вычисления факториала:  !( " + n1 + " )"  + "  =   " + factorial(n1));
-        System.out.println(" ");
-        System.out.println( " В последовательности чисел Фибоначчи " + n2 +" -й  елемент  равен:  " + GetFibo(n2));
-
         System.out.println("Task 1.5");
-        Sort.checkSorts();
-        int[] numbers = IntStream.generate(() -> (int) (Math.random()*(25331111+100000))-200000).limit(1000000).toArray();
+        int[] numbers = IntStream.generate(() -> {
+            return (int)(Math.random() * 2.5431111E7D) - 200000;
+        }).limit(1000000L).toArray();
         int n = numbers.length;
         System.out.println("Array length: " + numbers.length);
-        System.out.println("1-st element of array  before sort : "  + numbers[0]);
-        System.out.println("Last element of array  before sort : " + numbers[numbers.length-1]);
+        System.out.println("1-st element of array  before sort : " + numbers[0]);
+        System.out.println("Last element of array  before sort : " + numbers[numbers.length - 1]);
         long startTime = System.currentTimeMillis();
         Sort.bubble_Sort(numbers);
-        long spendTime = System.currentTimeMillis()-startTime;
-        for (int h = 0; h < n; h++)
-            System.out.println(numbers[h]+ " ");
+        long spendTime = System.currentTimeMillis() - startTime;
+
+        for(int h = 0; h < n; ++h) {
+            System.out.println(numbers[h] + " ");
+        }
+
         System.out.println("Array length: " + numbers.length);
         System.out.println("1-st element of array  after sort : " + numbers[0]);
-        System.out.println("Last element of array  after sort : " + numbers[numbers.length-1]);
+        System.out.println("Last element of array  after sort : " + numbers[numbers.length - 1]);
         System.out.println(" ");
         System.out.println(" Spend time for sorting: " + spendTime);
+        Sort.quickSort(numbers);
+        long quickTime = System.currentTimeMillis() - startTime;
 
+        for(int h = 0; h < n; ++h) {
+            System.out.println(numbers[h] + " ");
+        }
+
+        System.out.println("Array length: " + numbers.length);
+        System.out.println("1-st element of array  after sort : " + numbers[0]);
+        System.out.println("Last element of array  after sort : " + numbers[numbers.length - 1]);
+        System.out.println(" ");
+        System.out.println(" Spend time for sorting: " + quickTime);
+        Sort.selectionSort(numbers);
+        long selectionTime = System.currentTimeMillis() - startTime;
+
+        for(int h = 0; h < n; ++h) {
+            System.out.println(numbers[h] + " ");
+        }
+
+        System.out.println("Array length: " + numbers.length);
+        System.out.println("1-st element of array  after sort : " + numbers[0]);
+        System.out.println("Last element of array  after sort : " + numbers[numbers.length - 1]);
+        System.out.println(" ");
+        System.out.println(" Spend time for sorting: " + selectionTime);
+        Sort.insertionSort(numbers);
+        long insertionTime = System.currentTimeMillis() - startTime;
+
+        for(int h = 0; h < n; ++h) {
+            System.out.println(numbers[h] + " ");
+        }
+
+        System.out.println("Array length: " + numbers.length);
+        System.out.println("1-st element of array  after sort : " + numbers[0]);
+        System.out.println("Last element of array  after sort : " + numbers[numbers.length - 1]);
+        System.out.println(" ");
+        System.out.println(" Spend time for sorting: " + insertionTime);
+        Sort.heapSort(numbers);
+        long heapTime = System.currentTimeMillis() - startTime;
+
+        for(int h = 0; h < n; ++h) {
+            System.out.println(numbers[h] + " ");
+        }
+
+        System.out.println("Array length: " + numbers.length);
+        System.out.println("1-st element of array  after sort : " + numbers[0]);
+        System.out.println("Last element of array  after sort : " + numbers[numbers.length - 1]);
+        System.out.println(" ");
+        System.out.println(" Spend time for sorting: " + heapTime);
     }
-
-
 }
-
